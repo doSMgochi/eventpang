855cd1118988f405cbf8f068d46fddcefd5a986e
doSMgochi <aenarchi@gmail.com>
2024-08-29 14:50:47 +0900

    주석 처리

--- src/main/java/com/callor/eventpang/controller/NotificationController.java
+++ src/main/java/com/callor/eventpang/controller/NotificationController.java
@@ -1,29 +1,29 @@
-package com.callor.eventpang.controller;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-
-import com.callor.eventpang.service.EmailService;
-
-@Controller
-public class NotificationController {
-
-    private EmailService emailService;
-
-    @Autowired
-    public void setEmailService(EmailService emailService) {
-        this.emailService = emailService;
-    }
-
-    @RequestMapping("/send-email")
-    public String sendEmail(
-        @RequestParam("to") String to,         // 받는 사람의 이메일 주소
-        @RequestParam("subject") String subject,  // 이메일 제목
-        @RequestParam("content") String content   // 이메일 내용
-    ) {
-        emailService.sendEmail(to, subject, content);
-        return "success";
-    }
-}
+//package com.callor.eventpang.controller;
+//
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.stereotype.Controller;
+//import org.springframework.web.bind.annotation.RequestMapping;
+//import org.springframework.web.bind.annotation.RequestParam;
+//
+//import com.callor.eventpang.service.EmailService;
+//
+//@Controller
+//public class NotificationController {
+//
+//    private EmailService emailService;
+//
+//    @Autowired
+//    public void setEmailService(EmailService emailService) {
+//        this.emailService = emailService;
+//    }
+//
+//    @RequestMapping("/send-email")
+//    public String sendEmail(
+//        @RequestParam("to") String to,         // 받는 사람의 이메일 주소
+//        @RequestParam("subject") String subject,  // 이메일 제목
+//        @RequestParam("content") String content   // 이메일 내용
+//    ) {
+//        emailService.sendEmail(to, subject, content);
+//        return "success";
+//    }
+//}
--- src/main/java/com/callor/eventpang/controller/SmsController.java
+++ src/main/java/com/callor/eventpang/controller/SmsController.java
@@ -1,25 +1,25 @@
-package com.callor.eventpang.controller;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-
-import com.callor.eventpang.service.SmsService;
-
-@Controller
-public class SmsController {
-
-    private SmsService smsService;
-
-    @Autowired
-    public void setSmsService(SmsService smsService) {
-        this.smsService = smsService;
-    }
-
-    @RequestMapping("/send-sms")
-    public String sendSms(@RequestParam("to") String to, @RequestParam("body") String body) {
-        smsService.sendSms(to, body);
-        return "success";
-    }
-}
+//package com.callor.eventpang.controller;
+//
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.stereotype.Controller;
+//import org.springframework.web.bind.annotation.RequestMapping;
+//import org.springframework.web.bind.annotation.RequestParam;
+//
+//import com.callor.eventpang.service.SmsService;
+//
+//@Controller
+//public class SmsController {
+//
+//    private SmsService smsService;
+//
+//    @Autowired
+//    public void setSmsService(SmsService smsService) {
+//        this.smsService = smsService;
+//    }
+//
+//    @RequestMapping("/send-sms")
+//    public String sendSms(@RequestParam("to") String to, @RequestParam("body") String body) {
+//        smsService.sendSms(to, body);
+//        return "success";
+//    }
+//}
